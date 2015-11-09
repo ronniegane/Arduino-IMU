7c81bbdb7d678f23e62adef7df70fb1e1dde64eb
Ronnie Gane <ronniegane@gmail.com>
2015-11-10 01:52:07 +1300

    Cleaned up commenting and style

--- parselogs.py
+++ parselogs.py
@@ -1,9 +1,14 @@
 '''CSV data log processing for ArduIMU (Python 2.7)
 Takes in CSV files of format Datastring, Timestamp
 and extracts roll, pitch, yaw and other values
-then plots these as a time series.'''
+then plots these as a time series.
 
-''' TO DO:
+Created by Ronnie Gane and Beth Collis
+Last edit: 21/10/2015
+
+TO DO:
+- Encapsulate plotting into a function to reduce redundant code
+- Calculate velocity
 - Add table for Max/Min/Range/Average/Standard dev for various sensors
 - Make "try/except" more sensible
 - Filter GPS data: some points have latitude = longitude
@@ -18,7 +23,14 @@ import csv                              # reading and writing CSV files
 import re                               # regular expressions for searching strings
 from datetime import datetime           # for dealing with time values
 from numpy import mean
+import numpy as np
+from math import sin, cos
 
+'''
+currently unused imports
+from scipy.misc import imread		# for importing and working with image files
+import matplotlib.cbook as cbook	
+'''
 
 # Open file
 
@@ -29,8 +41,6 @@ outFile = open('cleaned data BMW session.csv', 'wb')   # Change to desired outpu
 # Create CSV "reader" and "writer" files
 inFile = open(myFile, 'rb')
 reader = csv.reader(inFile)
-
-
 writer = csv.writer(outFile)
 writer.writerow(["Time", "Lat", "Long", "Pitch", "Yaw", "Roll", "AccX", "AccY", "AccZ", "GyroX", "GyroY", "GyroZ"]) # Headers
 
@@ -85,7 +95,6 @@ for row in reader:
     endIndex = dataString.find('*')
     
     if (startIndex > -1 and endIndex > startIndex): # makes sure we are only checking complete logs
-
         
         '''Timestamp'''
         timeStamp = row[0]
@@ -146,7 +155,19 @@ for row in reader:
             gyroZ = gyroZList[-1]
         
         
+        # Transform coordinate system
+        rotAng = 20.0   # Transforming by rotation of positive 30 degrees about X axis 
+        tMatrix = np.array([[1.0, 0.0, 0.0], [0.0, cos(rotAng), -sin(rotAng)], [0.0, sin(rotAng), cos(rotAng)]])
         
+        # Transform pitch(X)/roll(Y)/yaw(Z)
+        [pitch, roll, yaw] = np.dot(tMatrix, [float(pitch), float(roll), float(yaw)])
+        
+        # Transform AccX/Y/Z
+        [accX, accY, accZ] = np.dot(tMatrix, [float(accX), float(accY), float(accZ)])
+        
+        
+        # Transform GyroX/Y/Z
+        [gyroX, gyroY, gyroZ] = np.dot(tMatrix, [float(gyroX), float(gyroY), float(gyroZ)])
         
         # Add to lists
         pitchList.append(float(pitch))
@@ -163,10 +184,9 @@ for row in reader:
         '''GPS'''
         latitude = float(row[2])
         longitude = float(row[3])
-        # Check for sensible values
+        # Uncomment to check for sensible values
         #print("Lat: %s" % latitude)
         #print("Long: %s" % longitude)
-
 
         # Latitude should be between -35 and -50 for NZ
         if -50 < latitude <-35:
@@ -185,8 +205,7 @@ for row in reader:
         writer.writerow([timeStamp, latitude, longitude, pitch, yaw, roll, accX, accY, accZ, gyroX, gyroY, gyroZ])
 
 
-# Do magic to acceleration values to produce tyre forces
-
+'''In the future this section will expand to include calcs of normal force'''
 print(timeList[:10])
 
 # Close files
@@ -205,9 +224,6 @@ datetimeList = [(datetime.strptime(x, timeFormat) - stripTime) for x in timeList
 deltaList = [x.seconds + x.microseconds/float(1000000) for x in datetimeList] # produce a list of gaps
 
 
-
-
-
 # Adjust roll values to be centred around zero rather than +- 180deg
 adjRollList = []
 for rollVal in rollList:
@@ -216,7 +232,22 @@ for rollVal in rollList:
     else:
         adjRollList.append(180-rollVal)
         
-# Calculate average, range of values for various 
+adjPitchList = []
+for pitchVal in pitchList:
+    if pitchVal < 0:
+        adjPitchList.append(180+pitchVal)
+    else:
+        adjPitchList.append(180-pitchVal)        
+        
+adjYawList = []
+for yawVal in yawList:
+    if yawVal < 0:
+        adjYawList.append(180+yawVal)
+    else:
+        adjYawList.append(180-yawVal)
+
+        
+# Calculate average, range of values for each variable
 avgDict = {
             "Roll": mean(adjRollList),
             "Yaw": mean(yawList),
@@ -242,15 +273,15 @@ maxDict = {
 }
 
 minDict = {
-            "Roll": max(adjRollList),
-            "Yaw": max(yawList),
-            "Pitch": max(pitchList),
-            "AccX": max(accXList),
-            "AccY": max(accYList),
-            "AccZ": max(accZList),
-            "GyroX": max(gyroXList),
-            "GyroY": max(gyroYList),
-            "GyroZ": max(gyroZList)
+            "Roll": min(adjRollList),
+            "Yaw": min(yawList),
+            "Pitch": min(pitchList),
+            "AccX": min(accXList),
+            "AccY": min(accYList),
+            "AccZ": min(accZList),
+            "GyroX": min(gyroXList),
+            "GyroY": min(gyroYList),
+            "GyroZ": min(gyroZList)
 }
 
 rangeDict = {}
@@ -260,7 +291,10 @@ for key in maxDict.keys():
 print("Raw average")
 print(avgDict)
 
-# Adjust data by mean value
+# Re-centre data around mean value
+# This fixes the weird offset values of some sensors
+# But assumes the mean acceleration will be zero - which 
+# should be true if we start and end at rest.
 accXList = accXList - avgDict["AccX"]
 accYList = accYList - avgDict["AccY"]
 accZList = accZList - avgDict["AccZ"]
@@ -269,10 +303,8 @@ gyroXList = gyroXList - avgDict["GyroX"]
 gyroYList = gyroYList - avgDict["GyroY"]
 gyroZList = gyroZList - avgDict["GyroZ"]
 
-
-# Filter GPS data
-
-
+# Modify pitch data
+#pitchList = [x-17.4 for x in pitchList]
 
 # Plotting
 print("Plotting %s data points" % len(timeList))
@@ -280,174 +312,206 @@ print("Plotting %s data points" % len(timeList))
 # Line plot of roll/pitch/yaw
 plt.plot(deltaList, pitchList, label='Pitch')
 plt.plot(deltaList, adjRollList, label='Roll')
-plt.plot(deltaList, yawList, label='Yaw')
-plt.title("Pitch / Roll / Yaw" + "\n" + shortTime, fontsize = 18)
+plt.plot(deltaList, adjYawList, label='Yaw')
+plt.title("Pitch / Roll / Yaw", fontsize = 18) #  + "\n" + shortTime
 plt.legend(loc=2)
 plt.grid()
 plt.xlabel("Time (seconds from start)")
 plt.ylabel("Degrees")
-
+plt.xlim(0,deltaList[-1])
 
 # Line plot of linear accelerations
 plt.figure()
 plt.plot(deltaList, accZList, label='Z-Accel', alpha = 0.7)
 plt.plot(deltaList, accYList, label='Y-Accel', alpha = 0.7)
 plt.plot(deltaList, accXList, label='X-Accel', alpha = 0.7)
-plt.title("Linear accelerations" + "\n" + shortTime, fontsize = 18)
+plt.title("Linear accelerations", fontsize = 18) #  + "\n" + shortTime
 plt.legend(loc=2)
 plt.grid()
 plt.xlabel("Time (seconds from start)")
-plt.ylabel("Units?")
+plt.ylabel("Acceleration")
+plt.xlim(0,deltaList[-1])
 
 # Line plot of rotational accelerations
 plt.figure()
 plt.plot(deltaList, gyroXList, label='X-Gyro', alpha = 0.7)
 plt.plot(deltaList, gyroYList, label='Y-Gyro', alpha = 0.7)
 plt.plot(deltaList, gyroZList, label='Z-Gyro', alpha = 0.7)
-plt.title("Rotational accelerations" + "\n" + shortTime, fontsize = 18)
+plt.title("Rotational accelerations", fontsize = 18) #  + "\n" + shortTime
 plt.legend(loc=2)
 plt.grid()
 plt.xlabel("Time (seconds from start)")
-plt.ylabel("Units?")
+plt.ylabel("Angular Acceleration")
+plt.xlim(0,deltaList[-1])
 
+'''
+#import image as background for scatter plots
+datafile = cbook.get_sample_data('trackmap.jpg')
+img = imread(datafile)
+'''
 
+# GPS 2D scatter plot
+# Coloured by the relevant variable
 
-
-# Possible other plots:
-
-# Line plot of velocity
-
-
-
-
-# GPS 2D data plot, lat and long
-# Colouring by other data
+# EULER ANGLES
+# Colouring by Pitch
 plt.figure()
-plt.scatter(longList, latList, s=100, c=pitchList)
-plt.clim(-20,50)
+plt.scatter(longList, latList, s=100, c=pitchList, cmap="RdYlBu")
+#plt.imshow(img, zorder=1) # Set background as a satellite image of track
+plt.clim(-30,30)
 plt.xlim(min(longList)-.001, max(longList)+.001)
 plt.ylim(min(latList)-.001, max(latList)+.001)
 ax = plt.gca()
 ax.get_xaxis().get_major_formatter().set_useOffset(False)  # This stops the tick marks of the axes going to "relative offset" scaling
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
+for tick in ax.get_xticklabels():
+    tick.set_rotation(90)                 #turns the x-axis labels vertically
+plt.title("Pitch variance", fontsize = 18) #  + "\n" + shortTime
 plt.colorbar(label = "Pitch")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.grid()
+plt.gcf().subplots_adjust(bottom=0.3)
 
-
+# Colouring by Yaw
 plt.figure()
-plt.scatter(longList, latList, s=100, c=yawList)
-
+plt.scatter(longList, latList, s=100, c=adjYawList, cmap="RdYlBu")
+plt.clim(-100,100)
 plt.xlim(min(longList)-.001, max(longList)+.001)
 plt.ylim(min(latList)-.001, max(latList)+.001)
 ax = plt.gca()
 ax.get_xaxis().get_major_formatter().set_useOffset(False)  # This stops the tick marks of the axes going to "relative offset" scaling
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
+for tick in ax.get_xticklabels():
+    tick.set_rotation(90)                 #turns the x-axis labels vertically
+plt.title("Yaw variance", fontsize = 18) #  + "\n" + shortTime
 plt.colorbar(label = "Yaw")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.grid()
+plt.gcf().subplots_adjust(bottom=0.3)
 
-
+# Colouring by Roll
 plt.figure()
-plt.scatter(longList, latList, s=100, c=adjRollList)
-plt.clim(-20,50)
+plt.scatter(longList, latList, s=100, c=adjRollList, cmap="RdYlBu")
+plt.clim(-90,90)
 plt.xlim(min(longList)-.001, max(longList)+.001)
 plt.ylim(min(latList)-.001, max(latList)+.001)
 ax = plt.gca()
 ax.get_xaxis().get_major_formatter().set_useOffset(False)  # This stops the tick marks of the axes going to "relative offset" scaling
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
+for tick in ax.get_xticklabels():
+    tick.set_rotation(90)                 #turns the x-axis labels vertically
+plt.title("Roll variance", fontsize = 18) #  + "\n" + shortTime
 plt.colorbar(label = "Roll")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.grid()
+plt.gcf().subplots_adjust(bottom=0.3)
 
-# Colouring by other data
+# LINEAR ACCELERATIONS
+# Colouring by x-accel
 plt.figure()
-plt.scatter(longList, latList, s=100, c=accXList)
+plt.scatter(longList, latList, s=100, c=accXList, cmap="RdYlBu")
 plt.clim(-2000,2000)
 plt.xlim(min(longList)-.001, max(longList)+.001)
 plt.ylim(min(latList)-.001, max(latList)+.001)
 ax = plt.gca()
 ax.get_xaxis().get_major_formatter().set_useOffset(False)  # This stops the tick marks of the axes going to "relative offset" scaling
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
+for tick in ax.get_xticklabels():
+    tick.set_rotation(90)                 #turns the x-axis labels vertically
 plt.colorbar(label = "AccX")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.grid()
+plt.gcf().subplots_adjust(bottom=0.3)
 
-
+# Colouring by y-accel
 plt.figure()
-plt.scatter(longList, latList, s=100, c=accYList)
+plt.scatter(longList, latList, s=100, c=accYList, cmap="RdYlBu")
 plt.clim(-4000,4000)
 plt.xlim(min(longList)-.001, max(longList)+.001)
 plt.ylim(min(latList)-.001, max(latList)+.001)
 ax = plt.gca()
 ax.get_xaxis().get_major_formatter().set_useOffset(False)  # This stops the tick marks of the axes going to "relative offset" scaling
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
+for tick in ax.get_xticklabels():
+    tick.set_rotation(90)                 #turns the x-axis labels vertically
 plt.colorbar(label = "accY")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.grid()
+plt.gcf().subplots_adjust(bottom=0.3)
 
-
+# Colouring by z-accel
 plt.figure()
-plt.scatter(longList, latList, s=100, c=accZList)
+plt.scatter(longList, latList, s=100, c=accZList, cmap="RdYlBu")
 plt.clim(-5000,5000)
 plt.xlim(min(longList)-.001, max(longList)+.001)
 plt.ylim(min(latList)-.001, max(latList)+.001)
 ax = plt.gca()
 ax.get_xaxis().get_major_formatter().set_useOffset(False)  # This stops the tick marks of the axes going to "relative offset" scaling
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
+for tick in ax.get_xticklabels():
+    tick.set_rotation(90)                 #turns the x-axis labels vertically
 plt.colorbar(label = "AccZ")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.grid()
+plt.gcf().subplots_adjust(bottom=0.3)
 
-# Colouring by other data
+# ROTATIONAL ACCELERATIONS
+# Colouring by x-rot
 plt.figure()
-plt.scatter(longList, latList, s=100, c=gyroXList)
+plt.scatter(longList, latList, s=100, c=gyroXList, cmap="RdYlBu")
 plt.clim(-200,200)
 plt.xlim(min(longList)-.001, max(longList)+.001)
 plt.ylim(min(latList)-.001, max(latList)+.001)
 ax = plt.gca()
 ax.get_xaxis().get_major_formatter().set_useOffset(False)  # This stops the tick marks of the axes going to "relative offset" scaling
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
+for tick in ax.get_xticklabels():
+    tick.set_rotation(90)                 #turns the x-axis labels vertically
 plt.colorbar(label = "GyroX")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.grid()
+plt.gcf().subplots_adjust(bottom=0.3)
 
-
+# Colouring by y-rot
 plt.figure()
-plt.scatter(longList, latList, s=100, c=gyroYList)
+plt.scatter(longList, latList, s=100, c=gyroYList, cmap="RdYlBu")
 plt.clim(-200,200)
 plt.xlim(min(longList)-.001, max(longList)+.001)
 plt.ylim(min(latList)-.001, max(latList)+.001)
 ax = plt.gca()
 ax.get_xaxis().get_major_formatter().set_useOffset(False)  # This stops the tick marks of the axes going to "relative offset" scaling
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
+for tick in ax.get_xticklabels():
+    tick.set_rotation(90)                 #turns the x-axis labels vertically
 plt.colorbar(label = "GyroY")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.grid()
+plt.gcf().subplots_adjust(bottom=0.3)
 
-
+# Colouring by z-rot
 plt.figure()
-plt.scatter(longList, latList, s=100, c=gyroZList)
+plt.scatter(longList, latList, s=100, c=gyroZList, cmap="RdYlBu")
 plt.clim(-200,200)
 plt.xlim(min(longList)-.001, max(longList)+.001)
 plt.ylim(min(latList)-.001, max(latList)+.001)
 ax = plt.gca()
 ax.get_xaxis().get_major_formatter().set_useOffset(False)  # This stops the tick marks of the axes going to "relative offset" scaling
 ax.get_yaxis().get_major_formatter().set_useOffset(False)
+for tick in ax.get_xticklabels():
+    tick.set_rotation(90)                 #turns the x-axis labels vertically
 plt.colorbar(label = "gyroZ")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 plt.grid()
+plt.gcf().subplots_adjust(bottom=0.3)
 
-# Maybe colour by velocity? Where would we calculate velocity from?
-
-
+# Display plots
 plt.show()
